9212d0ccdc959ed9cc4cbd69c01dd04fb9b3c3bf
Nikola Jurić <n.juric84@gmail.com>
2026-05-06 12:32:17 +0200

    Delete data directory

--- data/MyData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gdg_predavanje_3.ui.data
-
-data class MyData(
-    var ID: Int,
-    var title : String,
-    var description : String
-)
--- data/Note.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gdg_predavanje_3.ui.data
-
-import java.util.Date
-
-data class Note (
-    var title:String,
-    var description:String,
-    var date: Date
-)
